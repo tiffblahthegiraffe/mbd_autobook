9f793dc38b0e2916a3a3fa50d3fd9073582bbc24
Tiffany Sung <tiffblahthegiraffe@gmail.com>
2021-05-11 16:58:16 -0400

    check_schedule_and_book

--- f45_scheduler.py
+++ f45_scheduler.py
@@ -1,7 +1,17 @@
+import time
 from utility import MyDrive
+from selenium.common.exceptions import NoSuchElementException
 
 driver_path = '/Users/tiffanysung/Documents/chromedriver'
 mbd_url = 'https://clients.mindbodyonline.com/classic/mainclass?studioid=841970'
+DAILY_SCHEDULE_PREFERENCE = {'Mon': [5, ['7:15','6:15','8:15']],
+                             'Tue': [5, ['7:15','6:15','8:15']],
+                             'Wed': [5, ['7:15','6:15','8:15']],
+                             'Thu': [5, ['7:15','6:15','8:15']],
+                             'Fri': [3, ['7:15','6:15','8:15']],
+                             'Sat': [3, ['8:00','9:15', '10:']],
+                             'Sun': [2, ['9:30','10:30']]}
+DAY_TO_BOOK = 'Mon'
 
 def log_in(driver, usr_email, usr_pw):
     usr_email_field = driver.find_element('su1UserName',id=True)
@@ -14,7 +24,36 @@ def log_in(driver, usr_email, usr_pw):
         print(driver.find_element('LoginError', id=True).text)
     else:
         print('Log in successful')
+    return driver
 
+
+def check_schedule_and_book(schedule_table,DAY_TO_BOOK,DAILY_SCHEDULE_PREFERENCE):
+    row_count = 0
+    for row in schedule_table:
+        if DAY_TO_BOOK in row.text:
+            print('Booking for', row.text)
+            start_row = row_count + 1
+            end_row = row_count + 1 + DAILY_SCHEDULE_PREFERENCE[DAY_TO_BOOK][0]
+            time = schedule_table[start_row:end_row]
+            break
+        row_count += 1
+
+    preference_list = DAILY_SCHEDULE_PREFERENCE[DAY_TO_BOOK][1]
+
+    for i in preference_list:
+        print('try booking for', i)
+        for t in time:
+            print(t.text)
+            if i in t.text:
+                try:
+                    sign_up = t.find_element_by_class_name('SignupButton')
+                    print('There\'s still availibility for%s' % t.text[:10])
+                    return sign_up
+
+                except NoSuchElementException:
+                    print('Can\'t signup for %s. Not booked!' % t.text[:10])
+
+                break
 
 driver = MyDrive(driver_path)
 # Go on to mbd website
@@ -26,17 +65,25 @@ driver.click_button(sign_in_button)
 
 #log in
 with open('mbd_credential.txt','r') as file:
-    urs_email, urs_pw = file.readlines()
-    log_in(driver, urs_email, urs_pw)
+    usr_email, usr_pw = file.readlines()
+    log_in(driver, usr_email, usr_pw)
 
+time.sleep(2) # an important step to wait for page load
 # Direct to CLASS Tab
 class_tab = driver.find_element('tabTD7', id=True)
 driver.click_button(class_tab)
+time.sleep(1)
 
 # Refresh page by clicking Today
 today_button = driver.find_element('today-button', id=True)
 driver.click_button(today_button)
+time.sleep(1)
 
 # Get class schedule table
 table_path = "//table/tbody/tr/td/table[@id ='classSchedule-mainTable']/tbody/tr"
-schedule_table = driver.find_element(table_path, xpath=True)
+schedule_table = driver.find_element(table_path, xpath=True, multi=True)
+
+# Read table
+sign_up = check_schedule_and_book(schedule_table,DAY_TO_BOOK,DAILY_SCHEDULE_PREFERENCE)
+
+driver.close_drive()
